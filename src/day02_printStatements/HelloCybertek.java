ff4048686dbe2a887b35d75013bbf371770092de
Arasefe <ismail_yildirim69@yahoo.com>
2021-06-14 21:31:43 -0500

    13 June 2021

--- src/day02_printStatements/HelloCybertek.java
+++ src/day02_printStatements/HelloCybertek.java
@@ -8,6 +8,15 @@ public class HelloCybertek {
         System.out.println("Hello Batch 20");
         System.out.println("        I Love Java Programming Language");
 
+        main(new Float[]{new Float(1),new Float(2),new Float(3)});
+    }
+
+    public static void main(Float[] args) {
+        int multiply=1;
+        for (int i = 0; i < args.length; i++) {
+            multiply*=args[i];
+        }
+        System.out.println(multiply);
     }
 
 
