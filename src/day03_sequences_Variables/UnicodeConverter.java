ec419aa0c1a20db8d1b0caa3618eedf442fc2398
Arasefe <ismail_yildirim69@yahoo.com>
2021-06-17 14:32:17 -0500

    17June2021

---
+++ src/day03_sequences_Variables/UnicodeConverter.java
@@ -0,0 +1,37 @@
+package day03_sequences_Variables;
+
+import java.io.UnsupportedEncodingException;
+import java.io.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class UnicodeConverter {
+    public static void main(String[] args) {
+        String original = new String("A" + "\u004d" + "\u0045" + "\u0020" + "C");
+        //String original = new String("ME 04106 USA");
+
+
+        try {
+            byte[] utf8Bytes = original.getBytes("UTF8");
+
+
+            String roundTrip = new String(utf8Bytes, "UTF8");
+            System.out.println("roundTrip = " + roundTrip);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public static void printBytes(byte[] array, String name) {
+        for (int k = 0; k < array.length; k++) {
+            System.out.println(name + "[" + k + "] = " + "0x" +
+                    UnicodeFormatter.byteToHex(array[k]));
+        }
+
+    }
+}
