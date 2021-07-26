02db26c7fa283ecbec770838c73b4a6e035ee00b
Arasefe <ismail_yildirim69@yahoo.com>
2021-07-25 19:59:33 -0500

    July222021

--- src/day37_ArrayList/_5Duplicates.java
+++ src/day37_ArrayList/_5Duplicates.java
@@ -4,17 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class _4Duplicates {
+public class _5Duplicates {
 
     public static void main(String[] args) {
+        duplicates();
+
+
+    }
+    private static void duplicates(){
         ArrayList<Integer> list = new ArrayList<>();
         list.addAll( Arrays.asList(1,1,2,2,3,3,4,5,6,7,8,9,9,9,9,9,9,9,10,10,10,10,10,11,11,11) );
 
         list.removeIf(  p ->  Collections.frequency(list, p) == 1  );
         System.out.println(list);
-
-
-        System.out.println("=====================================================");
 
         ArrayList<String> names = new ArrayList<>();
         names.addAll( Arrays.asList("Anna", "Canada", "Bob", "David", "Lan", "Abida", "Ebrahim", "Farida") );
@@ -23,10 +25,8 @@ public class _4Duplicates {
 
         System.out.println(names);
 
-        System.out.println("============================================");
-
-        ArrayList<Integer> gardes = new ArrayList<>();
-        gardes.addAll( Arrays.asList(100, 90, 75, 85, 65, 85, 55, 45, 73, 73, 35, 47));
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.addAll( Arrays.asList(100, 90, 75, 85, 65, 85, 55, 45, 73, 73, 35, 47));
 
         ArrayList<Integer> gradeOfA = new ArrayList<>(); // 90 ~ 100
         ArrayList<Integer> gradeOfB = new ArrayList<>(); // 80 ~ 89
@@ -45,8 +45,6 @@ public class _4Duplicates {
 
         DO NOT use loop
          */
-
-
 
     }
 
--- src/day37_ArrayList/_7RemovePractice.java
+++ src/day37_ArrayList/_7RemovePractice.java
@@ -2,16 +2,21 @@ package day37_ArrayList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class _7RemovePractice {
 
     public static void main(String[] args) {
+        remove1();
+        remove2();
+        remove3();
+
+
+    }
+    private static void remove1(){
         ArrayList<Integer> list = new ArrayList<>();
         list.addAll(Arrays.asList(1,1,1,2,2,2,3,3,4,4,5,6,6,6,6,7,7));
-
-
         // remove all the numbers that are less than 5
-
         for(int i = 0; i <= list.size()-1; i++){
             if(  list.get(i) < 5  ){
                 list.remove(i);
@@ -20,7 +25,20 @@ public class _7RemovePractice {
 
         System.out.println(list);
 
+    }
 
+    private static void remove2(){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,1,1,2,2,2,3,3,4,4,5,6,6,6,6,7,7));
+        list.removeIf(p->p<5);
+        System.out.println(list);
+    }
+
+    private static void remove3(){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,1,1,2,2,2,3,3,4,4,5,6,6,6,6,7,7));
+            list.removeIf(p->p>3);
+            System.out.println(list);
 
     }
 
